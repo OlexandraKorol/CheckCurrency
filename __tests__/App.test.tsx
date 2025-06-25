0094a13eaff27643c1d996e09802249c6a1f5b64
Olexandra Korol <korol.olexandra@gmail.com>
2025-06-25 17:32:19 +0300

    set-up-jest-tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,56 @@
-/**
- * @format
- */
-
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import { BaseCurrency } from '../src/components/MessagesComponents';
+import { CurrencyListItem } from '../src/components/CurrencyListItem';
+import { CustomInput } from '../src/components/CustomInput';
+import { MainNavigation } from '../src/navigation/MainMavigation';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
-  });
+test('Base currency is visible', () => {
+  render(<BaseCurrency baseCurency={"EUR"} />);
+
+  const baseCurrencyElement = screen.getByTestId('base-currency');
+  expect(baseCurrencyElement).toBeTruthy();
+});
+
+test('Render list of currency', () => {
+  const onPress = jest.fn();
+  render(<CurrencyListItem isFavorite={false} code={'UAN'} value={0} onPress={onPress} />);
+
+  const baseCurrencyElement = screen.getByTestId('curency-list-item');
+  expect(baseCurrencyElement).toBeTruthy();
+
+});
+
+
+test('CustomInput works correctly', () => {
+  const handleChangeText = jest.fn();
+  render(<CustomInput value="UAH" onChangeText={handleChangeText} />);
+
+  const input = screen.getByTestId('custom-input');
+  fireEvent.changeText(input, 'jpy');
+  expect(handleChangeText).toHaveBeenCalledWith('jpy');
+});
+
+test('Navigates to Favorites screen when button is pressed', async () => {
+  render(<MainNavigation />);
+  expect(screen.getByText('All currencies')).toBeTruthy();
+  fireEvent.press(screen.getByText('Favorites'));
+
+  expect(await screen.findByText('Favorites')).toBeTruthy();
+});
+
+test('Check click updates favorite', () => {
+  const mockToggleFavorite = jest.fn();
+
+  render(
+    <CurrencyListItem
+      isFavorite={false}
+      code={'AED'}
+      value={3.67}
+      onPress={mockToggleFavorite}
+    />
+  );
+
+  fireEvent.press(screen.getByTestId('curency-list-item'));
+  expect(mockToggleFavorite).toHaveBeenCalled();
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./jestSetupFile.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@react-native|react-native|@react-native-async-storage/async-storage|@react-native-community/netinfo|@react-navigation)',
+  ],
 };
